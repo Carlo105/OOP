19f602935e30ba58c2c89c636b47f7b2444effed
Carlo105 <135827299+Carlo105@users.noreply.github.com>
2023-09-15 11:17:49 +0800

    Update TestMain.java

--- version1/TestMain.java
+++ version1/TestMain.java
@@ -4,14 +4,23 @@ public class TestMain {
     public static void main(String[] args) {
         HourlyEmployee emp = new HourlyEmployee(1, "Joe" , new myTime(23,2,2023), new myTime(3,3,2001), 45.2, 100.2);
         emp.displayInfo();
-        HourlyEmployee emp2 = new HourlyEmployee(2, "Bob" , new myTime(22,5,2018), new myTime(1,1,1991), 32.5, 100.2);
+        HourlyEmployee emp2 = new HourlyEmployee(2, 32.5, 100.2);
         emp2.displayInfo();
-
-        PieceWorkerEmployee emp3 = new PieceWorkerEmployee (3, "James" , new myTime(2,4,2018), new myTime(3,11,2000), 132.5, 100.2);
-        emp3.displayInfo();
-        PieceWorkerEmployee emp4 = new PieceWorkerEmployee (4, "Juan" , new myTime(6,6,2020), new myTime(19,11,2000), 40.5, 100.2);
+        HourlyEmployee emp3 = new HourlyEmployee();
+        
+        PieceWorkerEmployee emp4 = new PieceWorkerEmployee();
         emp4.displayInfo();
+        PieceWorkerEmployee emp5 = new PieceWorkerEmployee (3, 132.5, 100.2);
+        emp5.displayInfo();
+        PieceWorkerEmployee emp6 = new PieceWorkerEmployee (4, "Juan" , new myTime(6,6,2020), new myTime(19,11,2000), 40.5, 100.2);
+        emp6.displayInfo();
 
-        ComissionEmployee emp5 = new ComissionEmployee (4, "Juan" , new myTime(6,6,2020), new myTime(19,11,2000), 40.5, 100.2);
+        ComissionEmployee emp7 = new ComissionEmployee (5, "Juan" , new myTime(6,6,2020), new myTime(19,11,2000), 40.5, 100.2);
+        ComissionEmployee emp8 = new ComissionEmployee (6, 40.5, 100.2);
+        ComissionEmployee emp9 = new ComissionEmployee ();
+
+        BasePlusComissionEmployee emp10 = new BasePlusComissionEmployee (5, "Juan" , new myTime(6,6,2020), new myTime(19,11,2000), 40.5, 100.2);
+        BasePlusComissionEmployee emp11 = new BasePlusComissionEmployee (6, 40.5, 100.2);
+        BasePlusComissionEmployee emp12 = new BasePlusComissionEmployee ();
     }
 }
